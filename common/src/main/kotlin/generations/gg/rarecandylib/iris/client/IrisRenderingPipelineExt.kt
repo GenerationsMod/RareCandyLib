1d5fb577c7301745d761a164c9614cdb9a8bfc71
Waterpicker <waterpickerenternity@gmail.com>
2026-04-18 03:56:31 -0500

    purege iris attempt in favor of doing it in iris itself.

--- common/src/main/kotlin/generations/gg/rarecandylib/iris/client/IrisRenderingPipelineExt.kt
+++
@@ -1,7 +0,0 @@
-package generations.gg.rarecandylib.iris.client;
-
-import net.irisshaders.iris.targets.RenderTargets
-
-public interface IrisRenderingPipelineExt {
-    fun getRenderTargets(): RenderTargets
-}
--- common/src/main/kotlin/generations/gg/rarecandylib/iris/client/IrisShader.kt
+++
@@ -1,8 +0,0 @@
-package generations.gg.rarecandylib.iris.client;
-
-import net.irisshaders.iris.gl.framebuffer.GlFramebuffer
-import net.irisshaders.iris.pipeline.IrisRenderingPipeline
-
-public interface IrisShader {
-    fun fbo(pipeline: IrisRenderingPipeline): GlFramebuffer?
-}
